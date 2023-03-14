2ce37e7f9111837483e599eb86a2aa1798e04696
Ade Adegoke <ade.adegoke@conjure.co.uk>
2023-03-14 13:26:58 +0000

    Added files and file for the Coordinates

---
+++ BusTimes-TCA/BusTimes-TCA/API/Coordinates.swift
@@ -0,0 +1,10 @@
+//
+//  Coordinates.swift
+//  BusTimes-TCA
+//
+//  Created by Ade Adegoke on 14/03/2023.
+//
+
+import Foundation
+
+typealias Coordinate = (latitude: Double, longitude: Double)
--- BusTimes-TCA/BusTimes-TCA/Domain/BusStop.swift
+++ BusTimes-TCA/BusTimes-TCA/Domain/BusStop.swift
@@ -30,26 +30,3 @@ extension BusStop: Identifiable {
 struct Lines: Codable, Hashable {
     var name: String
 }
-
-struct ArrivalTime: Codable, Hashable {
-    var id: String
-    
-    var naptanId: String
-    var timeToStation: Int
-    var stationName: String
-    var lineName: String
-    var destinationName: String
-    
-    var timeInMinutes: String {
-        if timeToStation < 60 {
-            return "Due"
-        }
-        return "\(timeToStation / 60) mins"
-    }
-}
-
-extension ArrivalTime: Identifiable {
-    var identifier: String {
-        return id
-    }
-}
